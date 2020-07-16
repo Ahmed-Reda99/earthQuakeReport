c3c4b651da4d1486096f0e00e84f1fc4eada8c76
Ahmed-Reda99 <ahmedreda@fci.helwan.edu.eg>
2020-07-16 15:50:56 +0200

    first commit

---
+++ app/src/main/java/com/example/android/earthquakereport/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.android.earthquakereport;
+
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create a fake list of earthquake locations.
+        final ArrayList<earthQuake> earthquakes = QueryUtils.extractEarthquakes();
+
+        earthQuakeAdapter adapter = new earthQuakeAdapter(this,earthquakes);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        earthquakeListView.setAdapter(adapter);
+
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                earthQuake quake =earthquakes.get(position);
+
+                Uri webpage = Uri.parse(quake.getURL());
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
